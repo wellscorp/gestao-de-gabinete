7c1f0a782e5d79378eb04bdd82a50ffe1c5ccb8e
wellscorp <welton.s92@gmail.com>
2014-10-31 15:59:24 -0200

    Create inicio.php

---
+++ pags/inicio.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gestão de Contratos </title>
+
+	<script type="text/javascript" src="..\js/jquery.min.js" ></script>
+	<script type="text/javascript" src="..\js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="..\js/bootstrap.js"></script>
+	<script type="text/javascript" src="..\js/inicio.js"></script>
+	
+
+	<link href="..\css\bootstrap.min.css" rel="stylesheet">
+	<link href="..\css\layout.css" rel="stylesheet">
+</head>
+
+<body background="..\img\back_1.jpg">
+
+	<img align="left" src="../img/PRB.png" width="200px" style="position:relative; bottom:20px" />
+	<img align="right" src="../img/logo.png" width="100px" style="position:relative; bottom:20px; right:20px" />
+	
+	<!--		------------------------------------------------------------ -->
+
+	<!--
+	<div id="navi" class="container" >
+		<div class="row clearfix">
+			<div class="col-md-12 column">
+				<ul class="nav nav-pills " style="background-color:#C6D9FF; border-radius: 4px; opacity:0.8; width:100%;padding-right:40px ">
+					<li class="nav1">
+						<a href="#" id="novo" style="font-size:18px;  ">Cadastrar</a>
+					</li>
+					<li class="nav2">
+						<a href="#" id="buscar" style="font-size:18px">Consultar</a>
+					</li>
+					<li class="nav3">
+						<a href="#" id="config" style="font-size:18px">Configurações</a>
+					</li>					
+				</ul>
+			</div>
+		</div>
+	</div>
+	-->
+	<div style="position:relative; bottom:18px">
+		<div class="container" style="">
+		    <div class="row">
+				<div class="btn-group btn-group-justified">
+		            <div class="btn-group">
+		                <button type="button" class="btn btn-nav" name="novo">
+		                    <span class="glyphicon glyphicon-folder-close" id="novo"></span>
+		    			    <p>Cadastrar</p>
+		                </button>
+		            </div>
+		            <div class="btn-group">
+		                <button type="button" class="btn btn-nav" name="buscar">
+		                    <span class="glyphicon glyphicon-calendar"></span>
+		    			    <p id="buscar">Consultar</p>
+		                </button>
+		            </div>
+		            <div class="btn-group">
+		                <button type="button" class="btn btn-nav" name="config">
+		                    <span class="glyphicon glyphicon-globe"></span>
+		    			    <p id="config">Configurações</p>
+		                </button>
+		            </div>
+		        </div>
+			</div>
+		</div>
+
+		<!--		------------------------------------------------------------ -->
+
+		
+		<div id="container" >	
+	</div>
+
+</body>
+</html>
